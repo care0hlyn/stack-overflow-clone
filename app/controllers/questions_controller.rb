c95c0e103e722406c85b115fb8c1c61092cc5033
Carolyn Huynh & sterling houghton <student@epicodus.com>
2014-09-15 12:06:43 -0700

    Create question controller/authorize

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,6 @@
+class QuestionsController < ApplicationController
+
+  before_filter :authorize, only: [:edit, :update]
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
 
   root :to => 'questions#index'
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :sessions
   resources :users
+
   resources :questions do
     resources :answers
   end
   resources :votes
+
 end
